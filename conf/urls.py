1645d62784d367a60b5b4d5f8bf4d9bd2cd2eb6e
Your Name <you@example.com>
2015-05-17 19:35:10 +0300

    background change

---
+++ conf/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^', include('main.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
